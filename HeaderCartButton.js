ca2b1313937beffdc0857a1200912627219f3b75
RadhaNinave <101126042+RadhaNinave@users.noreply.github.com>
2023-01-18 21:30:30 +0530

    Add files via upload

--- HeaderCartButton.js
+++ HeaderCartButton.js
@@ -1,16 +1,22 @@
-import CartIcon from "../Cart/CartIcon";
-
-import classes from "./HeaderCartButton.module.css";
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 const HeaderCartButton = (props) => {
-  return<button className={classes.button} onClick={props.onClick}>
+ const cartCtx= useContext(CartContext);
+ const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
+  return curNumber +item.amount;
+ },0)
+  return (
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Cart Item</span>
-      <span className={classes.bagde}> 0 </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
-  
+  );
 };
 
 export default HeaderCartButton;
--- MealsItem.js
+++ MealsItem.js
@@ -1,20 +1,35 @@
-import classes from './MealsItem.module.css';
+
 import MealsItemForm from './MealsItemForm';
-const MealsItem = (props) =>{
-    const price = `$${props.price.toFixed(2)}`;
+import classes from './MealsItem.module.css';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
-    return (
-      <li className={classes.meal}>
-        <div>
-          <h3>{props.name}</h3>
-          <div className={classes.description}>{props.description}</div>
-          <div className={classes.price}>{price}</div>
-        </div>
-        <div>
-        <MealsItemForm id={props.id} />
-        </div>
-      </li>
-    );
+const MealItem = (props) => {
+  const cartCtx=useContext(CartContext);
+  const price = `$${props.price.toFixed(2)}`;
 
-}
-export default MealsItem;
+  const addToCartHandler = amount =>{
+    cartCtx.addItem({
+      id:props.id,
+      name:props.name,
+      price:props.price,
+      amount:amount
+    });
+
+  };
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealsItemForm onAddToCart = {addToCartHandler} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
